28eff8eaeabc36593b36c6a3cfe08c7cdd036dd0
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-10-13 03:41:16 +0700

    Update `scoper.inc.php` config

--- scoper.inc.php
+++ scoper.inc.php
@@ -9,4 +9,12 @@ require __DIR__ . '/vendor/autoload.php';
 /**
  * Generate the PHP-Scoper configuration.
  */
-return (new PHPScoperInc(__DIR__))->get();
+return (new PHPScoperInc(
+	__DIR__,
+	/**
+	 * The `openai-php` package exposes a global class.
+	 *
+	 * @see https://github.com/openai-php/client/blob/main/src/OpenAI.php
+	 */
+	['expose-global-classes' => false],
+))->get();
