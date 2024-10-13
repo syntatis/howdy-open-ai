ae6d72f295193f7a8e296f3c467dad47ecc2be11
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-10-13 13:24:41 +0700

    Update `Assistant` class with hook

--- app/Assistant.php
+++ app/Assistant.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace PluginName;
 
+use PluginName\Vendor\Codex\Contracts\Hookable;
+use PluginName\Vendor\Codex\Foundation\Hooks\Hook;
 use PluginName\Vendor\OpenAI\Client;
 
-class Assistant
+class Assistant implements Hookable
 {
 	private Client|null $client;
 
 	public function __construct(Client|null $client)
 	{
 		$this->client = $client;
+	}
+
+	public function hook(Hook $hook): void
+	{
+		$hook->addAction('init', [$this, 'addMenu']);
+	}
+
+	public function init(): void
+	{
+		// Do some cool stuff with the OpenAI client.
 	}
 }
